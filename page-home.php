c682aa7e1538fb9b8c4ff815413fc0c93fc57cc3
81joker <nehad.al.timimi@gmail.com>
2024-07-09 08:23:39 +0200

    add sidebar Seriveces

--- page-home.php
+++ page-home.php
@@ -9,7 +9,30 @@
                         Hero
                     </section>
                     <section class="services">
-                        Services
+                        <h2>Services</h2>
+                        <div class="container">
+                            <div class="services-item">
+                                <?php
+                                if( is_active_sidebar( 'services-1' ) ):
+                                    dynamic_sidebar( 'services-1' ); 
+                                    endif;
+                                ?>
+                            </div>
+                            <div class="services-item">
+                                <?php
+                                if( is_active_sidebar( 'services-2' ) ):
+                                    dynamic_sidebar( 'services-2' ); 
+                                    endif;
+                                ?>
+                            </div>
+                            <div class="services-item">
+                                <?php
+                                if( is_active_sidebar( 'services-3' ) ):
+                                    dynamic_sidebar( 'services-3' ); 
+                                    endif;
+                                ?>
+                            </div>
+                        </div>
                     </section>
                     <section class="home-blog">
                         <div class="container">
